252d83920bf407241b4fe7b381ea1e4cfccd7dd1
jungkh721 <jungkh721@icloud.com>
2025-07-16 17:58:25 +0900

    20250716 수정

---
+++ src/t2_AWT/T02_2.java
@@ -0,0 +1,63 @@
+package t2_AWT;
+
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+@SuppressWarnings("serial")
+public class T02_2 extends Frame implements	WindowListener, ActionListener {
+	
+	public T02_2() {
+		setTitle("AWT 프레임");
+		//setSize(300, 250); //프레임의 크기 (폭 높이 ): 픽셀단위
+		setBounds(300, 200, 400, 350);
+		
+		Label ldlTitle =new Label("레이블 테스트");
+		add(ldlTitle);
+		
+		Button btnExit =  new Button("Exit");
+		//btnAction.estlabel("Action");
+		
+		add(btnExit);
+		
+		
+		addWindowListener(this); //감시자
+		btnExit.addActionListener(this);
+		
+		setVisible(true);
+		
+	//------------------위쪽은 사용자 인터페이스구현(	UI), 아래쪽은 	method(동작)	처리------------
+	}
+	public static void main(String[] args) {
+		new	T02_2();
+	}
+	
+	@Override
+	public void windowOpened(WindowEvent e) {}
+	@Override
+	public void windowClosing(WindowEvent e) {
+		System.exit(0);
+	}
+	@Override
+	public void windowClosed(WindowEvent e) {}
+	@Override
+	public void windowIconified(WindowEvent e) {}
+	@Override
+	public void windowDeiconified(WindowEvent e) {}
+	@Override
+	public void windowActivated(WindowEvent e) {}
+	@Override
+	public void windowDeactivated(WindowEvent e) {}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.exit(0);
+		
+	}
+	
+
+}
